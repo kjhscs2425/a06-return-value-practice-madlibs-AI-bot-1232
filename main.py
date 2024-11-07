0cb899b44fb34c8d63205b60a0478a1950610611
AI-bot-1232 <nries2026@kehillahstudent.org>
2024-11-07 11:08:42 -0800

    I have finished the story

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 adjective0 = input("Give me an adjective: ")
 noun0 = input("Give me a noun: ")
+plural_noun = input("Give me a plural noun: ")
+aunt_person = input("Give me a person in the room (female): ")
+adjective1 = input("Give me an adjective: ")
+article_of_clothing = input("Give me an article of clothing: ")
+noun1 = input("Give me a noun: ")
+review_noun = input("Give me a noun: ")
+city = input("Give me a city: ")
+plural_noun2 = input("Give me a plural noun: ")
+adjective2 = input("Give me an adjective: ")
 
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
+
+
+story = f"There are many {adjective0} ways to choose a/n {noun0} to read. First, you could ask for recomendations from your friends and happ{plural_noun}. Just don't ask {aunt_person}- she only reads {adjective0} books with {article_of_clothing} ripping goddesses on the cover. If your friends and family are no help, try checking out {noun1}. Review in the {city} times. Id the {plural_noun2} features there are too {adjective0} for your taste, try somea little."
 print(story)
+
+full_story = f""
